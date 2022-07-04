bb028f6a6f8f1f6552022388b2aa1e01badc8610
dinhnguyenhuan <dinhnguyenhuan2021@gmail.com>
2022-07-04 10:49:56 +0700

    hello word

---
+++ chapter_01_Basic/clone_git_from_textbook_sources/02_clone_an_existing_git.py
@@ -0,0 +1,6 @@
+"""
+If you want to clone the repository into a directory named something other than libgit2, you can
+specify the new directory name as an additional argument:
+git clone https://github.com/libgit2/libgit2 mylibgit
+
+"""
