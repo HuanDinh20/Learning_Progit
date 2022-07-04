5a7e1d74ce94ec09eae4ee31f2de46ee69ef91c1
dinhnguyenhuan <dinhnguyenhuan2021@gmail.com>
2022-07-04 15:02:44 +0700

    chapter 01, part 07, aliases

---
+++ chapter_01_Basic/07_Get_Aliases.py
@@ -0,0 +1,9 @@
+"""
+
+A feature that can make your Git experience simpler, easier, and more familiar: aliases. Git doesn’t automatically
+infer your command if you type it in partially. If you don’t want to type the entire text of each of the Git
+commands, you can easily set up an alias for each command using git config.
+for example:
+git config --global alias.co checkout
+this mean, instead of typing git checkout,  you can type git co
+"""
