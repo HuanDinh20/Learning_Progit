ae9ed67d7613cf3309719e762fd0827be5c08a77
dinhnguyenhuan <dinhnguyenhuan2021@gmail.com>
2022-07-04 10:36:55 +0700

    getting_git_repository.py

---
+++ chapter_01_Basic/getting_git_repository.py
@@ -0,0 +1,15 @@
+"""
+You typically obtain a Git repository in one of two ways:
+1. You can take a local directory that is currently not under version control, and turn it into a Git
+repository
+2. You can clone an existing Git repository from elsewhere.
+***** Initializing a Repository in an Existing Directory *****
+cd to/your/directories
+and then
+git init
+
+This creates a new subdirectory named .git that contains all of your necessary repository files — a
+Git repository skeleton.
+If you want to start version-controlling existing files (as opposed to an empty directory), you should
+probably begin tracking those files and do an initial commit
+"""
