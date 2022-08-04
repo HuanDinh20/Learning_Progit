756982c94ec1ee1f36cd88a2e44d5fee56ee4d22
dinhnguyenhuan <dinhnguyenhuan2021@gmail.com>
2022-08-04 17:23:21 +0700

    take an example for merge conflic

---
+++ chapter_02_Git_Branching/06_Basic_Merge_Conflix.py
@@ -0,0 +1,15 @@
+"""
+Occasionally, this process doesn’t go smoothly. If you changed the same part of the same file
+differently in the two branches you’re merging, Git won’t be able to merge them cleanly. If your fix
+for issue #53 modified the same part of a file as the hotfix branch, you’ll get a merge conflict.
+take example like:
+
+apple
+red
+green
+blue
+
+- now create 2 branches name: dev1, dev2
+
+
+"""
